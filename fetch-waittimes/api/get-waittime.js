f15e8ba18bb83a08393a3bc486d8eec954fa9ca4
Toadtoad <kingdomkeepersguy@gmail.com>
2021-07-23 15:14:43 -0700

    added logging

--- fetch-waittimes/api/get-waittime.js
+++ fetch-waittimes/api/get-waittime.js
@@ -4,11 +4,15 @@ const dl = "../waittimes/dl.json";
 const ca = "../waittimes/ca.json";
 
 module.exports = (req, res) => {
+  console.log("query");
+  console.log(req.query);
   const { id, time, park } = req.query;
   let parkObj;
   if (park.includes("california")) {
+    console.log("using california adventure");
     parkObj = ca;
   } else {
+    console.log("using disneyland");
     parkObj = dl;
   }
   let timesISO = Object.keys(parkObj);
@@ -17,7 +21,11 @@ module.exports = (req, res) => {
     times.push(parseISO(timesISO[i]));
   }
   let closestTime = closestTo(parseISO(time), times);
+  console.log("closest time");
+  console.log(closestTime);
   let result = parkObj[closestTime.toISOString()][id]
+  console.log("result");
+  console.log(result);
 
   res.json({
     ...result
